276bac123e3caaa35af8bfdc58c89df208544952
pasa ismihan <pasa_13142@hotmail.com>
2023-04-26 22:02:31 +0300

    guncel

--- api/server.js
+++ api/server.js
@@ -1,3 +1,12 @@
 // server için gerekli olanları burada ayarlayın
 
 // posts router'ını buraya require edin ve bağlayın
+
+const express = require("express");
+const server = express();
+const postRouter = require("./posts/posts-router");
+server.use(express.json());
+
+server.use("/api/posts", postRouter);
+
+module.exports = server;
